64f9ecef8a81468d7d714822d7ea2039fcbcd20c
Stephan Hoermann <srhoermann@gmail.com>
2015-02-09 16:40:11 +1100

    Got products working.

--- macros/src/main/scala/au/com/cba/omnia/answer/macros/ExtractorMacro.scala
+++ macros/src/main/scala/au/com/cba/omnia/answer/macros/ExtractorMacro.scala
@@ -26,49 +26,64 @@ object ExtractorMacro {
   def impl[A : c.WeakTypeTag](c: Context): c.Expr[Extractor[A]] = {
     import c.universe._
 
-    val typ = c.universe.weakTypeOf[A]
+    val targetType: Type = c.universe.weakTypeOf[A]
 
     /** Fail compilation with nice error message. */
     def abort(msg: String) =
-      c.abort(c.enclosingPosition, s"Can't create Extractor for $typ: $msg")
+      c.abort(c.enclosingPosition, s"Can't create Extractor for $targetType: $msg")
 
+    /** Process an individual column. */
+    def processColumn(typ: Type, position: Int): Tree = {
+      // Primitives plus String
+      val booleanType           = c.universe.weakTypeOf[Boolean]
+      val byteType              = c.universe.weakTypeOf[Byte]
+      val doubleType            = c.universe.weakTypeOf[Double]
+      val floatType             = c.universe.weakTypeOf[Float]
+      val intType               = c.universe.weakTypeOf[Int]
+      val longType              = c.universe.weakTypeOf[Long]
+      val stringType            = c.universe.weakTypeOf[String]
 
-    // Primitives plus String
-    val booleanType           = c.universe.weakTypeOf[Boolean]
-    val byteType              = c.universe.weakTypeOf[Byte]
-    val doubleType            = c.universe.weakTypeOf[Double]
-    val floatType             = c.universe.weakTypeOf[Float]
-    val intType               = c.universe.weakTypeOf[Int]
-    val longType              = c.universe.weakTypeOf[Long]
-    val stringType            = c.universe.weakTypeOf[String]
+      val bigDecimalType        = c.universe.weakTypeOf[java.math.BigDecimal]
+      val dateType              = c.universe.weakTypeOf[java.sql.Date]
+      val jodaDateTimeType      = c.universe.weakTypeOf[org.joda.time.DateTime]
+      val jodaLocalDateType     = c.universe.weakTypeOf[org.joda.time.LocalDate]
+      val jodaLocalDateTimeType = c.universe.weakTypeOf[org.joda.time.LocalDateTime]
+      val jodaLocalTimeType     = c.universe.weakTypeOf[org.joda.time.LocalTime]
+      val timeType              = c.universe.weakTypeOf[java.sql.Time]
+      val timestampType         = c.universe.weakTypeOf[java.sql.Timestamp]
 
-    val bigDecimalType        = c.universe.weakTypeOf[java.math.BigDecimal]
-    val dateType              = c.universe.weakTypeOf[java.sql.Date]
-    val jodaDateTimeType      = c.universe.weakTypeOf[org.joda.time.DateTime]
-    val jodaLocalDateType     = c.universe.weakTypeOf[org.joda.time.LocalDate]
-    val jodaLocalDateTimeType = c.universe.weakTypeOf[org.joda.time.LocalDateTime]
-    val jodaLocalTimeType     = c.universe.weakTypeOf[org.joda.time.LocalTime]
-    val timeType              = c.universe.weakTypeOf[java.sql.Time]
-    val timestampType         = c.universe.weakTypeOf[java.sql.Timestamp]
-
-    typ match {
-      case `booleanType`           => c.Expr[Extractor[A]](q"Extractor(rs => rs.boolean(1))")
-      case `byteType`              => c.Expr[Extractor[A]](q"Extractor(rs => rs.byte(1))")
-      case `doubleType`            => c.Expr[Extractor[A]](q"Extractor(rs => rs.double(1))")
-      case `floatType`             => c.Expr[Extractor[A]](q"Extractor(rs => rs.float(1))")
-      case `intType`               => c.Expr[Extractor[A]](q"Extractor(rs => rs.int(1))")
-      case `longType`              => c.Expr[Extractor[A]](q"Extractor(rs => rs.long(1))")
-      case `stringType`            => c.Expr[Extractor[A]](q"Extractor(rs => rs.string(1))")
-      case `bigDecimalType`        => c.Expr[Extractor[A]](q"Extractor(rs => rs.bigDecimal(1))")
-      case `dateType`              => c.Expr[Extractor[A]](q"Extractor(rs => rs.date(1))")
-      case `jodaDateTimeType`      => c.Expr[Extractor[A]](q"Extractor(rs => rs.jodaDateTime(1))")
-      case `jodaLocalDateType`     => c.Expr[Extractor[A]](q"Extractor(rs => rs.jodaLocalDate(1))")
-      case `jodaLocalDateTimeType` => c.Expr[Extractor[A]](q"Extractor(rs => rs.jodaLocalDateTime(1))")
-      case `jodaLocalTimeType`     => c.Expr[Extractor[A]](q"Extractor(rs => rs.jodaLocalTime(1))")
-      case `timeType`              => c.Expr[Extractor[A]](q"Extractor(rs => rs.time(1))")
-      case `timestampType`         => c.Expr[Extractor[A]](q"Extractor(rs => rs.timestamp(1))")
-      case _                       => abort(s"$typ is not an expected type")
+      typ match {
+        case `booleanType`           => q"rs.boolean($position)"
+        case `byteType`              => q"rs.byte($position)"
+        case `doubleType`            => q"rs.double($position)"
+        case `floatType`             => q"rs.float($position)"
+        case `intType`               => q"rs.int($position)"
+        case `longType`              => q"rs.long($position)"
+        case `stringType`            => q"rs.string($position)"
+        case `bigDecimalType`        => q"rs.bigDecimal($position)"
+        case `dateType`              => q"rs.date($position)"
+        case `jodaDateTimeType`      => q"rs.jodaDateTime($position)"
+        case `jodaLocalDateType`     => q"rs.jodaLocalDate($position)"
+        case `jodaLocalDateTimeType` => q"rs.jodaLocalDateTime($position)"
+        case `jodaLocalTimeType`     => q"rs.jodaLocalTime($position)"
+        case `timeType`              => q"rs.time($position)"
+        case `timestampType`         => q"rs.timestamp($position)"
+        case _                       => abort(s"$typ is not an expected type")
+      }
     }
+
+    val targetTypes = targetType.declarations.sorted.toList collect {
+      case sym: TermSymbol if sym.isVal && sym.isCaseAccessor => sym.typeSignatureIn(targetType)
+    }
+
+    val extractors =
+      if (targetTypes.isEmpty) processColumn(targetType, 1)
+      else {
+        val parts = targetTypes.zipWithIndex.map { case (typ, i) => processColumn(typ, i + 1) }
+        q"(..$parts)"
+      }
+
+    c.Expr[Extractor[A]](q"Extractor(rs => $extractors)")
   }
 }
 
