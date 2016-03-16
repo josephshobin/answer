edcf83265210be1bc5abd6d62364ecdaa2eeb7a4
Rowan Davies <Rowan.Davies@cba.com.au>
2016-03-16 11:25:13 +1100

    WIP.

--- core/src/main/scala/au/com/cba/omnia/answer/DB.scala
+++ core/src/main/scala/au/com/cba/omnia/answer/DB.scala
@@ -26,6 +26,8 @@ import au.com.cba.omnia.omnitool.{Result, Ok, ResultantOps, ToResultantMonadOps,
 import au.com.cba.omnia.omnitool.ResultantMonad._
 import au.com.cba.omnia.omnitool.ResultantMonadSyntax._
 
+import au.com.cba.omnia.answer.{DB => DBResult}
+
 /** Configuration required to run a `DB` instance. 
   * 
   * @param jdbcUrl  : jdbc url to the database
@@ -39,9 +41,10 @@ case class DBConfig(jdbcUrl: String, user: String, password: String, driver: Opt
   val name = s"$jdbcUrl/$user"
 }
 
+
+/** Builds a DB monad for any Result-related monad R */
 class DBT[R[_]](implicit R: RelMonad[Result, R]) {
 
-  //  val R = ResultantMonad[R]
   implicit def Resultant: ResultantMonad[R] = ResultantMonad[R](R)
 
   /**
@@ -62,11 +65,11 @@ class DBT[R[_]](implicit R: RelMonad[Result, R]) {
       * @return Resultant `A` of this DB action.
       */
     def run(connection: Connection): R[A] = {
-      R.point(SDB(connection))   >>= ((sdb: scalikejdbc.DB) =>
+      R.point(SDB(connection)) >>= ((sdb: scalikejdbc.DB) =>
         (
-          R.point(sdb.begin())     >>
-          sdb.withinTx(action)     >>= ((a: A) =>
-          R.point(sdb.commit())    .map(_ =>
+          R.point(sdb.begin())  >>
+          sdb.withinTx(action)  >>= ((a: A) =>
+          R.point(sdb.commit()) .map(_ =>
           a
         )))    // Proposed new parenthesis convention for "for-like" code
           .onException { R.point(sdb.rollback()) }   //TODO:  logger.warn(s"...");
@@ -85,14 +88,70 @@ class DBT[R[_]](implicit R: RelMonad[Result, R]) {
       )(conn => run(conn))
   }
 
-  object DB extends ResultantOps[DB] with ToResultantMonadOps  with DbROps[DB] {
+  object DB extends ResultantOps[DB] with ToResultantMonadOps with DbROps[DB] {
 
-    implicit val DbRel = ??? //new RelMonad.SelfR[DB]()   //  TODO
+    // A generalised "self-composing" RelMonad(L, DB) instance for all L with RelMonad(L, R), instantiated below.
+    def lowerRel[L[_]](implicit LRelR: RelMonad[L, R]) = new RelMonad[L, DB] {
+      def rPoint[A](v: => L[A])                     = DB[A](_ => LRelR.rPoint(v))
+      def rBind[A, B](dba: DB[A])(f: L[A] => DB[B]) = DB[B](c => LRelR.rBind(dba.action(c))(a => f(a).action(c)))
+    }
+ 
+    val DbRel:     RelMonad[DB, DB]     = new RelMonad.SelfR[DB]     // For DbROps[DB]
 
-    implicit val RRel: RelMonad[R, DB] = new RelMonad[R, DB] {  // TODO
-      def rPoint[A](v: => R[A]): DB[A] = DB[A](_ => v)
-      def rBind[A, B](ma: DB[A])(f: R[A] => DB[B]): DB[B] =
-        DB(c => f(ma.action(c)).action(c))
+    val ResultRel: RelMonad[Result, DB] = lowerRel[Result](R)        // For ResultantOps[DB]
+    implicit val monad:     ResultantMonad[DB]   = ResultantMonad(ResultRel)
+  }
+
+  /** Convenient operations that you can do on a `RelMonad[DB, _]`. */
+  trait DbROps[M[_]] {
+    implicit val DbRel: RelMonad[DB, M]
+    implicit val monad: Functor[M]        // Seems required for map below (build this into RelMonad ?)
+
+    def ask[A](f: DBSession => A): M[A] =
+      DbRel.rPoint(DB(s => R.point(f(s))))
+
+    /**
+      * Runs the specified sql query expecting a single row as response.
+      * 
+      * If no row matches the query it returns `None`. If the result is not single, an `Error` 
+      * is returned.
+      */
+    def querySingle[A : Extractor](sql: SQL[Nothing, NoExtractor]): M[Option[A]] =
+      ask(implicit session => sql.map(implicitly[Extractor[A]].extract).single.apply())
+
+    /**
+      * Runs the specified sql query and get the first row as response.
+      * 
+      * If no row matches the query it returns `None`.
+      */
+    def queryFirst[A : Extractor](sql: SQL[Nothing, NoExtractor]): M[Option[A]] =
+      query[A](sql).map(_.headOption)
+
+    /** Runs the specified sql query */
+    def query[A : Extractor](sql: SQL[Nothing, NoExtractor]): M[Traversable[A]] =
+      ask(implicit session => sql.map(implicitly[Extractor[A]].extract).traversable().apply())
+
+    /** Create a new SQL connection or get a pooled connection. Library users are responsible
+      * for adding the appropriate jdbc drivers as a dependency.
+      *
+      * @param config: database configuration for the connection
+      *
+      * @return The SQL connection 
+      */
+    def connection(config: DBConfig): Result[java.sql.Connection] = Result.safe {
+      if (!ConnectionPool.isInitialized(config.name)) {
+        lazy val derivedDriver = config.jdbcUrl.split(":").take(3) match {
+          case Array("jdbc", "sqlserver", _) => "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+          case Array("jdbc", "hsqldb"   , _) => "org.hsqldb.jdbcDriver"
+          case Array("jdbc", "oracle"   , _) => "oracle.jdbc.OracleDriver"
+          case Array("jdbc", "mysql"    , _) => "com.mysql.jdbc.Driver"
+          case Array("jdbc", unknown    , _) => throw new Exception(s"Unsupported jdbc driver: $unknown")
+          case _                             => throw new Exception(s"Invalid jdbc url: ${config.jdbcUrl}")
+        }
+        Class.forName(config.driver.fold(derivedDriver)(identity))
+        ConnectionPool.add(config.name, config.jdbcUrl, config.user, config.password)
+      }
+      ConnectionPool.borrow(config.name)
     }
   }
 }
--- core/src/main/scala/au/com/cba/omnia/answer/package.scala
+++ core/src/main/scala/au/com/cba/omnia/answer/package.scala
@@ -1,11 +1,11 @@
 package au.com.cba.omnia
 
-import scalaz._, Scalaz._, Free.Trampoline
+//import scalaz._, Scalaz._, Free.Trampoline
 
-import org.slf4j.{Logger, LoggerFactory}
+//import org.slf4j.{Logger, LoggerFactory}
 
 import au.com.cba.omnia.omnitool.{Result, ResultantMonad}
-import au.com.cba.omnia.omnitool.TrampolineResult.TrampoResult
+//import au.com.cba.omnia.omnitool.TrampolineResult.TrampoResult
 
 // Instaniate DBT  //was: forming the monad: `DBSession => Trampoline[Result[_]]`
 package object answer extends DBT[Result] //(LoggerFactory.getLogger("au.com.cba.omnia.answer"))
